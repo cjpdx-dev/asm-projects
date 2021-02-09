bcbfc5431ece9f9340b3d92f0f59305c76d1712a
Chris Jacobs <cjpdx.dev@gmail.com>
2021-02-09 01:00:24 -0500

    almost done with project 3

--- Project3/Project3.asm
+++ Project3/Project3.asm
@@ -25,184 +25,306 @@ UPPER_LIMIT_2 = -1
 
 .data
 
-nameAndProgramTitle		BYTE		"Project 3 - Data Validation, Looping, and Constants - By Chris Jacobs",0	
+nameAndProgramTitle		BYTE		"Project 3 - Min, Max and Average Calculator - By Chris Jacobs",0	
+extraCreditPrompt1		BYTE		"EC 1: Number the lines during user input. Increment the line number only for valid number entries.",0
+extraCreditPrompt2		BYTE		"EC 2: Calculate and display the average as a decimal-point number, rounded to the nearest .01",0
 
 namePrompt				BYTE		"Please enter your name: ",0
 userNameBuffer			BYTE		21 DUP(0)
-userNameByteCount		DWORD		?  ; not sure if we need this, unless we wanted to handle user induced overflow
+userNameByteCount		DWORD		?
 userGreeting			BYTE		"Hello ",0
 
 userInstructions1		BYTE		"Please enter a series of numbers in the ranges of -200 to -100 (inclusive) and -50 to -1 (inclusive).",0
 userInstructions2		BYTE		"When you are finished, enter a positive number to end your input.",0
 userInstructions3		BYTE		"You will then be given the number of inputs, the minimum and maximum value you entered, and the average.",0
 
-lineNumberLabel			BYTE		"Line ",0
-lineNumberSpacer		BYTE		": ",0
-
 userInput				SDWORD		?
 
-numberOfInputs			DWORD		0
+numInputs				SDWORD		0
 sumOfInputs				SDWORD		0
 minValue				SDWORD		?
 maxValue				SDWORD		?
 averageValue			SDWORD		?
+averageValueRemainder	SDWORD		?
 
-outOfRangeErrorPrompt	BYTE		"You entered a value outside the range of -200 to -100 or -50 to -1. Please try again, or enter a positive number to quit.",0
+outOfRangeErrorPrompt	BYTE		"You entered a value outside the range of [-200 to -100] or [-50 to -1]. Please try again, or enter a positive number to quit.",0
+noInputsDetectedPrompt	BYTE		"You didn't input any values.",0
 
 loopTerminatedPrompt	BYTE		"Data Input Completed. Here are your results: ",0
 numInputsPrompt			BYTE		"Number of inputs: ",0
 minValuePrompt			BYTE		"Minimum Value: ",0
 maxValuePrompt			BYTE		"Maximum Value: ",0
 sumOfInputsPrompt		BYTE		"Sum of all inputs: ",0
-averageValuePrompt		BYTE		"Average of all inputs: ",0
+averageValuePrompt		BYTE		"Rounded average of all inputs: ",0
+averageInDecimalPrompt  BYTE		"Average of all inputs in decimal form: ",0
 
 goodbyePrompt			BYTE		"Goodbye!",0
+
+; Extra Credit
+lineNumberLabel			BYTE		"Line ",0
+lineNumberSpacer		BYTE		": ",0
+lineNumber				DWORD		1
+
+averageValueInDecimal	REAL4		?
+averageDecimalRemainder	REAL4		?
+averageDecimalPart		REAL4		?
 
 .code
 main PROC
 
-	; display program title and programmer name
-	MOV		edx, OFFSET nameAndProgramTitle
+	; Display program title and programmer name, and extra credit prompts
+	MOV		EDX, OFFSET nameAndProgramTitle
+	CALL	WriteString
+	CALL	Crlf
+	CALL	Crlf
+	MOV		EDX, OFFSET extraCreditPrompt1
+	CALL	WriteString
+	CALL	Crlf
+	CALL	Crlf
+	MOV		EDX, OFFSET extraCreditPrompt2
 	CALL	WriteString
 	CALL	Crlf
 
-	; prompt user to enter their name,
-	MOV		edx, OFFSET namePrompt
+	; Prompt user to enter their name
+	CALL	Crlf
+	MOV		EDX, OFFSET namePrompt
 	CALL	WriteString
 
-	; read the name and store in a null terminated byte array
-	MOV		edx, OFFSET userNameBuffer
-	MOV		ecx, SIZEOF	userNameBuffer
+	; Read the name and store in a null terminated byte array
+	MOV		EDX, OFFSET userNameBuffer
+	MOV		ECX, SIZEOF	userNameBuffer
 	CALL	ReadString
-	MOV		userNameByteCount, eax
+	MOV		userNameByteCount, EAX
 
-	; greet the user
+	; Greet the user
 	CALL	Crlf
-	MOV		edx, OFFSET userGreeting
+	MOV		EDX, OFFSET userGreeting
 	CALL	WriteString
-	MOV		edx, OFFSET userNameBuffer
+	MOV		EDX, OFFSET userNameBuffer
 	CALL	WriteString
 	CALL	Crlf
 
-	; give the user instructions
+	; Give the user instructions
 	CALL	Crlf
-	MOV		edx, OFFSET userInstructions1
+	MOV		EDX, OFFSET userInstructions1
 	CALL	WriteString
 	CALL	Crlf
-	MOV		edx, OFFSET userInstructions2
+	CALL	Crlf
+	MOV		EDX, OFFSET userInstructions2
 	CALL	WriteString
 	CALL	Crlf
-	MOV		edx, OFFSET userInstructions3
+	CALL	Crlf
+	MOV		EDX, OFFSET userInstructions3
 	CALL	WriteString
 	Call	Crlf
 
-	; enter the input loop
-	_inputLoop:
-		
-		; display the line number
-		MOV		edx, OFFSET lineNumberLabel		
+	; Enter the input loop
+	_InputLoop:
+
+		; Display the line number (Extra Credit)
+		CALL	Crlf
+		MOV		EDX, OFFSET lineNumberLabel		
 		CALL	WriteString
-		MOV		eax, numberOfInputs
+		MOV		EAX, lineNumber
 		CALL	WriteDec
-		MOV		edx, OFFSET lineNumberSpacer
+		MOV		EDX, OFFSET lineNumberSpacer
 		CALL	WriteString
 
-
-		; get the user input
+		; Get the user's input
 		CALL	ReadInt							
-		MOV		userInput, eax
+		MOV		userInput, EAX
 
+		; Exit loop if value was positive
+		JNS		_EndInputLoop					
 
-		; exit loop if value was positive
-		JNS		_endInputLoop					
-
-
-		; check if userInput < LOWER_LIMIT_1
-		MOV		eax, LOWER_LIMIT_1
-		CMP		userInput, eax
+		; Check if userInput < LOWER_LIMIT_1
+		MOV		EAX, LOWER_LIMIT_1
+		CMP		userInput, EAX
 		JL		_PromptInputOutOfRange	
 		
+		; Check if userInput > UPPER_LIMIT_1
+		; If it is, JMP to _CheckLowerLimit2 to make sure that userInput > LOWER_LIMIT_2
+		MOV		EAX, UPPER_LIMIT_1
+		CMP		userInput, EAX
+		JG		_CheckLowerLimit2			
+		JMP		_UpdateValues
 
-		; check if userInput > UPPER_LIMIT_1
-		; if it is, JMP to _checkLowerLimit2 to make sure that userInput > LOWER_LIMIT_2
-		MOV		eax, UPPER_LIMIT_1
-		CMP		userInput, eax
-		JG		_checkLowerLimit2			
-		JMP		_updateValues
+		; Check if userInput > LOWER_LIMIT_2
+		_CheckLowerLimit2:
+			MOV		EAX, LOWER_LIMIT_2
+			CMP		userInput, EAX
+			JL		_PromptInputOutOfRange		; userInput is greater than -100 but less than -50, so handle invalid input
+			JMP		_UpdateValues				; Else, update the running values
 
-
-		; check if userInput > LOWER_LIMIT_2
-		_checkLowerLimit2:
-			MOV		eax, LOWER_LIMIT_2
-			CMP		userInput, eax
-			JL		_promptInputOutOfRange		; userInput is greater than -100 but less than -50, so handle invalid input
-			JMP		_updateValues
-
-
+		; Prompt the user that their value is out of the required range
 		_PromptInputOutOfRange:
 			CALL	Crlf
-			MOV		edx, OFFSET outOfRangeErrorPrompt
+			MOV		EDX, OFFSET outOfRangeErrorPrompt
 			CALL	WriteString
 			CALL	Crlf
-			JMP		_inputLoop
+			JMP		_InputLoop
+				
+		; Update the values for minValue, maxValue, numberOfInputs, and averageValue
+		_UpdateValues:
+
+			; Increment the line number (Extra Credit)
+			MOV		EDX, lineNumber
+			INC		EDX
+			MOV		lineNumber, EDX
+
+			; Increment the number of inputs
+			MOV		EDX, numInputs
+			INC		EDX
+			MOV		numInputs, EDX
+
+			; Detect whether the input is the first input
+			CMP		EDX, 1
+			JE		_DetectedFirstInput				; If numberOfInputs = 1, JMP to _DetectedFirstInput
+
+			; Check if we have a new minValue
+			MOV		EDX, minValue
+			CMP		userInput, EDX
+			JG		_DetectNewMaxValue
+			MOV		EDX, userInput
+			MOV		minValue, EDX
+
+
+		; Check if we have a new maxValue
+		_DetectNewMaxValue:
+
+			MOV		EDX, maxValue
+			CMP		userInput, EDX
+			JL		_AddUserInputToTotal
+			MOV		EDX, userInput
+			MOV		maxValue, EDX
+
+
+		; Add new userInput to running total
+		_AddUserInputToTotal:
+			MOV		EDX, sumOfInputs
+			ADD		EDX, userInput
+			MOV		sumOfInputs, EDX
+			JMP		_InputLoop						; Repeat the loop
+
+
+		; Initialize minValue, maxValue and sumOfInputs to the value of the first input
+		_DetectedFirstInput:
+			MOV		EDX, userInput
+			MOV		minValue, EDX
+			MOV		maxValue, EDX
+			MOV		sumOfInputs, EDX
+			JMP		_InputLoop						; Repeat the loop
 				
 
-		; update the values for minValue, maxValue, numberOfInputs, and averageValue
-		_updateValues:
-			MOV		edx, numberOfInputs
-			INC		edx
-			MOV		numberOfInputs, edx
-			CMP		edx, 1
-			JE		_detectedFirstInput			; if numberOfInputs = 1, JMP to _detectedFirstInput
+	; Alert the user that they quit the input phase, calculate average (normal form and decimal form),
+	_EndInputLoop:
+		
+		; Check to see if the user entered zero inputs.
+		MOV		EAX, numInputs
+		CMP		EAX, 0
+		JE		_NoInputsDetected					; If numInputs == 0, JMP to _NoInputsDetected
 
-
-			; check if we have a new minValue
-			MOV		edx, minValue
-			CMP		userInput, edx
-			JG		_detectNewMaxValue
-			MOV		edx, userInput
-			MOV		minValue, edx
-
-			_detectNewMaxValue:
-				MOV		edx, minValue
-				CMP		userInput, edx
-				JL		_addUserInputToTotal
-				MOV		edx, userInput
-				MOV		minValue, edx
-
-			_addUserInputToTotal:
-				
-
-
-
-
-
-			JMP		_inputLoop					; repeat the loop
-
-
-			; initialize minValue, maxValue and sumOfInputs to the value of the first input
-			_detectedFirstInput:
-				MOV		EDX, userInput
-				MOV		minValue, EDX
-				MOV		maxValue, EDX
-				MOV		sumOfInputs, EDX
-				JMP		_inputLoop				; repeat the loop
-				
-
-	; end of loop, display results and then exit program
-	_endInputLoop:
 		CALL	Crlf
-		MOV		edx, OFFSET loopTerminatedPrompt
-		CALL	WriteString
+		CALL	Crlf
+		MOV		EDX, OFFSET loopTerminatedPrompt 
+		CALL	WriteString							; Display input completion message
+		CALL	Crlf
+		CALL	Crlf
 
+		; Calculate rounded non-decimal average
+		MOV		EDX, 0
+		MOV		EAX, sumOfInputs
+		CDQ
+		IDIV	numInputs
+		MOV		averageValue, EAX
 
-	; say goodbye and exit to operating system
-	_ExitProgram:
-		MOV		edx, OFFSET goodbyePrompt
-		CALL	WriteString
-		Invoke ExitProcess,0					; exit to operating system
+		; Store information for decimal average (Extra Credit)
+		MOV		averageValueInDecimal, EAX
+		MOV		averageDecimalRemainder, EDX
+
+		; Decide whether to round the non-decimal
+		CMP		EDX, -2
+		JNE		_DisplayResults
+		MOV		EAX, averageValue
+		DEC		EAX
+		MOV		averageValue, EAX
+
+		; Calculate decimal average (Extra Credit)
+		MOV		EDX, 0
+		MOV		EAX, averageValueInDecimal
+		CDQ
+		IDIV	averageDecimalRemainder
+		ADD		averageValueInDecimal, EAX
 
 		
+
+	; Display results and then exit program
+	_DisplayResults:
+
+		; Display numInputsPrompt and numInputs
+		MOV		EDX, OFFSET numInputsPrompt
+		CALL	WriteString
+		MOV		EAX, numInputs
+		CALL	WriteDec
+		CALL	Crlf
+
+		; Display minValuePrompt and minValue
+		CALL	Crlf
+		MOV		EDX, OFFSET minValuePrompt
+		CALL	WriteString
+		MOV		EAX, minValue
+		CALL	WriteInt
+		CALL	Crlf
+
+		; Display maxValuePrompt and maxValue
+		CALL	Crlf
+		MOV		EDX, OFFSET	maxValuePrompt
+		CALL	WriteString
+		MOV		EAX, maxValue
+		CALL	WriteInt
+		CALL	Crlf
+
+		; Display sumOfInputsPrompt and sumOfInputs
+		CALL	Crlf
+		MOV		EDX, OFFSET sumOfInputsPrompt
+		CALL	WriteString
+		MOV		EAX, sumOfInputs
+		CALL	WriteInt
+		CALL	Crlf
+
+		; Display averageValuePrompt and averageValue
+		CALL	Crlf
+		MOV		EDX, OFFSET averageValuePrompt
+		CALL	WriteString
+		MOV		EAX, averageValue
+		CALL	WriteInt
+		CALL	Crlf
+
+		; Display averageInDecimalPrompt and averageValueInDecimal
+		CALL	Crlf
+		MOV		EDX, OFFSET averageInDecimalPrompt
+		CALL	WriteString
+		MOV		EAX, averageValueInDecimal
+		CALL	WriteInt
+		CALL	Crlf
+
+
+	; Say goodbye and exit to operating system
+	_ExitProgram:
+		CALL	Crlf
+		MOV		EDX, OFFSET goodbyePrompt
+		CALL	WriteString
+		Invoke	ExitProcess,0					; Exit to operating system
+
+	; Prompts user that no inputs were detected, then jumps to _ExitProgram
+	_NoInputsDetected:
+		CALL	Crlf
+		MOV		EDX, OFFSET	noInputsDetectedPrompt
+		CALL	WriteString
+		CALL	Crlf
+		CALL	Crlf
+		JMP		_ExitProgram
+
 main ENDP
 
 ; (insert additional procedures here)
